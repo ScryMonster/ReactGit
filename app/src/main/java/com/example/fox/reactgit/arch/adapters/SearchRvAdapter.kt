f0b61b5c95d132341df289e165dbb7235769c06b
neonfox <lblackneonl@gmail.com>
2018-04-25 01:02:59 +0300

    adapter

--- app/src/main/java/com/example/fox/reactgit/arch/adapters/SearchRvAdapter.kt
+++ app/src/main/java/com/example/fox/reactgit/arch/adapters/SearchRvAdapter.kt
@@ -4,11 +4,10 @@ import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
 import com.example.fox.reactgit.dto.Organisation
+import com.example.fox.reactgit.utils.inflate
 
 class SearchRvAdapter(private val list:List<Organisation>,click:View.OnClickListener) : RecyclerView.Adapter<SearchRvAdapter.OrganisationViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): OrganisationViewHolder {
-
-    }
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int) = OrganisationViewHolder(parent?.inflate())
 
     override fun getItemCount()  = list.size
 
